b6a49ad010899fe2d37e0fe08b93b9052f696f54
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-07-03 13:20:58 -0400

    getExamScores

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,48 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores;
+
+    public Student(String firstName, String lastName, Double[] testScores){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = new ArrayList<>(testScores.length);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getNumberOfExamsTaken(){
+        return this.examScores.size();
+    }
+
+    public String getExamScores() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for(double val : this.examScores){
+            stringBuilder.append("Exam " + getNumberOfExamsTaken() + " -> " + this.examScores.set(0, val) + "\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    public void addExamScore(double examScore){
+        this.examScores.add(examScore);
+    }
+
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,36 @@
 package io.zipcoder;
 
-public class StudentTest {
+import org.junit.Test;
 
+public class StudentTest {
+    @Test
+    public void testStudentExam() {
+        // : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = {100.0, 95.0, 123.0, 96.0};
+        Student student = new Student(firstName, lastName, examScores);
+
+        // When
+        String output = student.getExamScores();
+
+        // Then
+        System.out.println(output);
+    }
+
+    @Test
+    public void addExamScoreTest() {
+        // : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = {};
+        Student student = new Student(firstName, lastName, examScores);
+
+        // When
+        student.addExamScore(100.0);
+        String output = student.getExamScores();
+
+        // Then
+        System.out.println(output);
+    }
 }

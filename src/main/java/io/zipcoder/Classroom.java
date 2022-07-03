fb1f4989ae545fccb68afc09fab7c40cf8c26540
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-07-03 16:46:50 -0400

    Classroom setup

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,23 @@
 package io.zipcoder;
 
 public class Classroom {
+    private Student[] students;
+    private int maxNumberOfStudents;
+
+    public Classroom(int maxNumberOfStudents){
+        this.maxNumberOfStudents = maxNumberOfStudents;
+    }
+    public Classroom(Student[] student){
+
+    }
+    public Classroom(){
+        this(30);
+    }
+    public Student getStudent(){
+        return null;
+    }
+    public double getAverageExamScore(){
+        getStudent();
+        return 0;
+    }
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,25 @@
 package io.zipcoder;
 
+import org.junit.Test;
+
 public class ClassroomTest {
+
+    @Test
+    public void getAverageExamScore(){
+        // : Given
+        Double[] s1Scores = { 100.0, 150.0 };
+        Double[] s2Scores = { 225.0, 25.0 };
+
+        Student s1 = new Student("student", "one", s1Scores);
+        Student s2 = new Student("student", "two", s2Scores);
+
+        Student[] students = {s1,s2};
+        Classroom classroom = new Classroom(students);
+
+        // When
+        double output = classroom.getAverageExamScore();
+
+        // Then
+        System.out.println(output);
+    }
 }
